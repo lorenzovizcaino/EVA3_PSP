74c46c29810244f2666b11a714642d2410c5b186
Antonio Lorenzo Vizcaino <lorenzovizcaino@gmail.com>
2023-05-10 19:50:41 +0200

    10/5/23_19:50

---
+++ src/boletin2Sockets/ejercicio6/Cliente.java
@@ -0,0 +1,14 @@
+package boletin2Sockets.ejercicio6;
+/*
+Realiza un programa servidor que escuche en el puerto 12345. Cada vez que se conecte un
+cliente se creará un nuevo hilo para atenderlo. Se mostrará en la consola del servidor la
+dirección IP y el puerto remoto del cliente que se conecta. Se deberá notificar también cuando
+un cliente se desconecte.
+En el hilo que atiende al cliente, se reciben cadenas de caracteres que, mientras sean distintas
+de “*”, se devolverán al cliente transformadas a mayúsculas.
+ */
+public class Cliente {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/boletinRepasoSockets1/ej4/ej2_Servidor.java
@@ -0,0 +1,43 @@
+package boletinRepasoSockets1.ej4;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+Usando Sockets TCP realiza un programa cliente que introduzca cadenas por teclado
+hasta introducir un asterisco. Las cadenas se enviarán a un programa servidor. El
+programa servidor aceptará la conexión de un único cliente y por cada cadena recibida
+le devolverá al cliente el número de caracteres de la misma. El programa servidor
+finalizará cuando reciba un asterisco como cadena.
+
+ */
+public class ej2_Servidor {
+    public static void main(String[] args) throws IOException {
+        int puerto=6000;
+        int cuentaCaracteres;
+        ServerSocket servidor=new ServerSocket(puerto);
+        Socket cliente=servidor.accept();
+        String cadena = "";
+        InputStream entrada=null;
+        DataInputStream flujoEntrada=null;
+        OutputStream salida=null;
+        DataOutputStream flujoSalida=null;
+        while(!cadena.equalsIgnoreCase("*")){
+            entrada=cliente.getInputStream();
+            flujoEntrada=new DataInputStream(entrada);
+            cadena=flujoEntrada.readUTF();
+            cuentaCaracteres=cadena.length();
+            salida=cliente.getOutputStream();
+            flujoSalida=new DataOutputStream(salida);
+            flujoSalida.writeInt(cuentaCaracteres);
+
+        }
+        flujoSalida.close();
+        salida.close();
+        flujoEntrada.close();
+        entrada.close();
+        cliente.close();
+        servidor.close();
+    }
+}
